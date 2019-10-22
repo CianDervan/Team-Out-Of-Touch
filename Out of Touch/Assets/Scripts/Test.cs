ea0e1646bed6f6bc5cff9cef9b59bb61fb6e1c11
jonanobonano <c16743131>
2019-10-22 23:29:24 +0100

    nothindoin

---
+++ Out of Touch/Assets/Scripts/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Test : MonoBehaviour
+{
+    public GameObject spinMe;
+    public float RotateSpeed = 30f;
+    public Transform target;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        spinMe.transform.Rotate(-Vector3.up.normalized * RotateSpeed * Time.deltaTime);
+        Debug.Log("I should be spinning");
+    }
+    
+    public void SpinMe()
+    {
+        //spinMe.transform.Rotate(-Vector3.up * RotateSpeed * Time.deltaTime);
+        //spinMe.transform.Rotate(0,RotateSpeed, 0);
+        
+        float step =  RotateSpeed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        
+        Debug.Log("I should be spinning");
+    }
+}
