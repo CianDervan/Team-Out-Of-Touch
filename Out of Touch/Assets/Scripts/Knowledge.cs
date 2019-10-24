b929ec383dd18f88b2e55d35b44f1e70a483c1be
CianDervan <44029453+CianDervan@users.noreply.github.com>
2019-10-24 21:37:25 +0100

    Menus
    Main, Pause menu done & knowledge started

---
+++ Out of Touch/Assets/Scripts/Knowledge.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Knowledge : MonoBehaviour
+{
+    public void pressKnowledge()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
---
+++ Out of Touch/Assets/Scripts/ResumeButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResumeButton : MonoBehaviour
+{
+    public void pressResume()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+
+
+}
