4d3eb70f42cc5274b4f06d9c74cc34924e37d196
Dazariath <c17366881@mydit.ie>
2019-10-25 19:10:31 +0100

    furthered the code for touch screen integration

--- Out of Touch/Assets/Scripts/TouchInput.cs
+++ Out of Touch/Assets/Scripts/TouchInput.cs
@@ -17,42 +17,34 @@ public class TouchInput : MonoBehaviour
 
 #if UNITY_EDITOR
 
-        if (Input.touchCount > 0)
+        if (Input.GetMouseButton(0)||Input.GetMouseButtonDown(0)|| Input.GetMouseButtonUp(0))
         {
             touchesOld = new GameObject[touchList.Count];
             touchList.CopyTo(touchesOld);
             touchList.Clear();
 
-            foreach (Touch touch in Input.touches)
+            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, touchInputMask))
             {
-                Ray ray = GetComponent<Camera>().ScreenPointToRay(touch.position);
-                RaycastHit hit;
+                GameObject recipient = hit.transform.gameObject;
+                touchList.Add(recipient);
 
-                if (Physics.Raycast(ray, out hit, touchInputMask))
+                if (Input.GetMouseButtonDown(0))
                 {
-                    GameObject recipient = hit.transform.gameObject;
-                    touchList.Add(recipient);
-
-                    if (touch.phase == TouchPhase.Began)
-                    {
-                        recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
-                    }
-                    if (touch.phase == TouchPhase.Ended)
-                    {
-                        recipient.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
-                    }
-
-                    if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
-                    {
-                        recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
-                    }
-                    if (touch.phase == TouchPhase.Canceled)
-                    {
-                        recipient.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
-                    }
+                    recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
                 }
-
+                if (Input.GetMouseButton(0))
+                {
+                    recipient.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+                if (Input.GetMouseButtonUp(0))
+                {
+                    recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
             }
+
+            
             /*foreach (GameObject g in touchesOld)
             {
                 if (!touchList.Contains(g))
