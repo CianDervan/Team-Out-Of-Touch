578b5b6d90bcb65e8b2e50da9f5520be7309db9a
Dazariath <c17366881@mydit.ie>
2019-10-29 19:14:09 +0000

    Added scripts and stuff

---
+++ Out of Touch/Assets/Scripts/Know System/ShowBasicKnowledge.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowBasicKnowledge : MonoBehaviour
+{
+    public Image BasicKnowledgeImage;
+
+    private ButtonTouchInput SBK_ButtonTouchInput;
+
+    void Start()
+    {
+        SBK_ButtonTouchInput = GetComponent<ButtonTouchInput>();
+
+        BasicKnowledgeImage = GetComponent<Image>();
+    }
+
+    public void BasicKnowledgeIsAcquired(bool BasicKnowledgeAcquired = false)
+    {
+        if (BasicKnowledgeAcquired == true)
+        {
+            BasicKnowledgeImage.enabled = true;
+            Debug.Log("Advanced Knowledge Acquired!");
+        }
+    }
+}
