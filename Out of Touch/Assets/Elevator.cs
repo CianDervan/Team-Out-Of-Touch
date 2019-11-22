1a44686126db4233010d50059c0e8d1e1371e5b2
Dazariath <darraghmnolan@gmail.com>
2019-11-22 03:48:54 +0000

    added a bunch of shit

---
+++ Out of Touch/Assets/Elevator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public Rigidbody ElevatorRB;
+
+    private void OnTriggerStay(Collider NoSelfCollision)
+    {
+        
+    }
+}
